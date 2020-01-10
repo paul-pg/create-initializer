10eb078155ec2d86b627da28a206ec40154c371b
Paul <p.geraets@outlook.com>
2020-01-10 14:48:20 +0100

    async fix

--- setup.js
+++ setup.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
-const createProject = require('npm-initializer-template');
+const createProject = require('npm-initializer-template')
 
-createProject({
-  workingDirectory: './', // Directory where the project will be cloned
-  npmPackage: {
-    name: '@paul-pg/create-initializer', // NPM Package to clone
-    // version: '1.12.1', // Optional, default to latest
-  },
-});
+async function main() {
+  await createProject({
+    workingDirectory: './', // Directory where the project will be cloned
+    npmPackage: {
+      name: '@paul-pg/create-initializer', // NPM Package to clone
+      // version: '1.12.1', // Optional, default to latest
+    },
+  });
+}
+main();
