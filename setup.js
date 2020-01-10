6a4b4eea9e81880dd667900c748831dd505488e0
Paul <p.geraets@outlook.com>
2020-01-10 10:32:02 +0100

    console log

--- setup.js
+++ setup.js
@@ -1,11 +1,3 @@
 #!/usr/bin/env node
-import { createProject } from 'npm-initializer-template';
 
-console.log('testing123');
-createProject({
-  workingDirectory: './', // Directory where the project will be cloned
-  npmPackage: {
-    name: '@paul-pg/initializer', // NPM Package to clone
-    // version: '1.12.1', // Optional, default to latest
-  },
-});
+console.log('this works');
