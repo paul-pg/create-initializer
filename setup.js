9bb8c5505043bea3e5ba4f5b375b8741dad2f1d0
Paul <p.geraets@outlook.com>
2020-01-10 15:16:55 +0100

    no message

--- setup.js
+++ setup.js
@@ -1,23 +1,14 @@
 #!/usr/bin/env node
-const createProject = require('npm-initializer-template');
+import { createProject } from 'npm-initializer-template';
 
 console.log('create project');
-
-const agent = createProject({
-  workingDirectory: './', // Directory where the project will be cloned
-  npmPackage: {
-    name: '@paul-pg/create-initializer', // NPM Package to clone
-    // version: '1.12.1', // Optional, default to latest
-  },
-});
-
-// async function main() {
-//   await createProject({
-//     workingDirectory: './', // Directory where the project will be cloned
-//     npmPackage: {
-//       name: '@paul-pg/create-initializer', // NPM Package to clone
-//       // version: '1.12.1', // Optional, default to latest
-//     },
-//   });
-// }
-agent;
+async function main() {
+  await createProject({
+    workingDirectory: './', // Directory where the project will be cloned
+    npmPackage: {
+      name: '@paul-pg/create-initializer', // NPM Package to clone
+      // version: '1.12.1', // Optional, default to latest
+    },
+  });
+}
+main();
