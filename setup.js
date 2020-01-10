977d3f94c53ccab190b1415ceaf337f9e033c964
Paul <p.geraets@outlook.com>
2020-01-10 10:03:59 +0100

    testing

--- setup.js
+++ setup.js
@@ -1,12 +1,10 @@
-// test
-
-import { createProject } from 'npm-initializer-template';
+// import { createProject } from 'npm-initializer-template';
 console.log('testing')
 
-await createProject({
-  workingDirectory: './', // Directory where the project will be cloned
-  npmPackage: {
-    name: '@paul-pg/initializer', // NPM Package to clone
-    // version: '1.12.1', // Optional, default to latest
-  },
-});
+// await createProject({
+//   workingDirectory: './', // Directory where the project will be cloned
+//   npmPackage: {
+//     name: '@paul-pg/initializer', // NPM Package to clone
+//     // version: '1.12.1', // Optional, default to latest
+//   },
+// });
