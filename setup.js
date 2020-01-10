4e5c38d794c4f02d0bdfc6bbbea2f143c9ab9825
Paul <p.geraets@outlook.com>
2020-01-10 16:26:04 +0100

    no message

--- setup.js
+++ setup.js
@@ -4,6 +4,18 @@ const shell = require('shelljs');
 
 const path = './';
 
+console.log('1. cloning project ⏳');
 shell.cd(path);
-shell.exec('git clone https://gitlab.com/GreenhouseGroup/creativedevelopment/opel-chat.git');
-console.log('done ⏰');
+shell.exec('git clone https://github.com/paul-pg/create-initializer.git');
+console.log('2. done ⏰');
+console.log('3. doing other important stuff 🕹');
+shell.cd('/create-initializer');
+
+// origin van geclonede project veranderen! anders pushen naar bestaand project
+// eventueel parameters in package.json aanpassen
+// https://confluence.atlassian.com/bitbucketserver/basic-git-commands-776639767.html
+
+// 1. gitlab project met bin/index.js
+// 2. index.js cloned hetzelfde gitlab project
+// 3. changes maken aan nieuw gekopieerd lokaal project
+// als gitlab niet werkt, via @freshfruit
