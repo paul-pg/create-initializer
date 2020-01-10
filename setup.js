d90420581e17b5a1ee0818229aa978db2e60352b
Paul <p.geraets@outlook.com>
2020-01-10 10:38:17 +0100

    fs test

--- setup.js
+++ setup.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
+const fs = require('fs-extra');
 
 console.log('this works');
+
+fs.copy('/tmp/myfile', '/tmp/mynewfile')
+  .then(() => console.log('success!'))
+  .catch(err => console.error(err));
