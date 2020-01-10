b2bf9c15d1f615652703d5dbf098386970510e4a
Paul <p.geraets@outlook.com>
2020-01-10 15:31:09 +0100

    no message

--- setup.js
+++ setup.js
@@ -1,14 +1,9 @@
 #!/usr/bin/env node
-const createProject = require('npm-initializer-template');
 
-console.log('create project');
-async function main() {
-  await createProject({
-    workingDirectory: './', // Directory where the project will be cloned
-    npmPackage: {
-      name: '@paul-pg/create-initializer', // NPM Package to clone
-      // version: '1.12.1', // Optional, default to latest
-    },
-  });
-}
-main();
+const shell = require('shelljs');
+
+const path = './folder';
+
+shell.cd(path);
+shell.exec('git clone https://github.com/paul-pg/create-initializer');
+console.log('done');
