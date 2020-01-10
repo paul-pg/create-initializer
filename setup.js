9420e4abbdfa91a620035a4ec912ab1e36c1b7c3
Paul <p.geraets@outlook.com>
2020-01-10 14:44:58 +0100

    test

--- setup.js
+++ setup.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
-const fs = require('fs-extra');
+const createProject = require('npm-initializer-template');
 
-console.log('this works');
-
-fs.copy('https://github.com/paul-pg/create-initializer.git', '/tmp/mynewfile')
-  .then(() => console.log('success!'))
-  .catch(err => console.error(err));
+createProject({
+  workingDirectory: './', // Directory where the project will be cloned
+  npmPackage: {
+    name: '@paul-pg/create-initializer', // NPM Package to clone
+    // version: '1.12.1', // Optional, default to latest
+  },
+});
